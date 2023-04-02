bb2d44ff288b35dfadc9cd4de5f538329b36a58f
Vanshita Shah <shahv7309@gmail.com>
2023-04-02 11:18:14 +0530

    form validation is added

--- validation.js
+++ validation.js
@@ -1,15 +1,42 @@
-export default function validateForm() {
-  let formFormate = /^\s*(?!\s$)\S.*\S\s*$/;
-  if (productName.value == "" || !productName.value.match(formFormate)) {
-    alert("Name must be filled out");
+export default function isValid(name, price,photo) {
+  console.log(price);
+  if (name == "" || name.trim() == "") {
+    alert("Invalid name");
     return false;
-  } else if (description.value == "" || !description.value.match(formFormate)) {
-    alert("description must be filled out");
+  } else if (!name.match(/^[A-Za-z][ A-Za-z0-9_/()-]*$/)) {
+    alert("Name should not contain any such characters");
     return false;
-  } else if (price.value == "" || !price.value.match(formFormate)) {
-    alert("price must be filled out");
+  } else if (price.includes("e")) {
+    alert("please enter valid price");
     return false;
-  } else {
-    return true;
+  }else if(!isFileValid(photo)){
+    alert("file invalid");
+    return false;
   }
+  console.log("here");
+  return true;
+}
+
+function isFileValid(productPhoto) {
+  const idxDot = productPhoto.value.lastIndexOf(".") + 1;
+  const extFile = productPhoto.value
+    .substr(idxDot, productPhoto.value.length)
+    .toLowerCase();
+  if(extFile===""){
+    return true
+  }
+  if (extFile == "jpg" || extFile == "jpeg" || extFile == "png") {
+    if (productPhoto.files[0].size > 200000) {
+      alert("File size should be <200KB");
+      productPhoto.value = "";
+      imgPreview.src = "./images/img-2.png";
+      return false;
+    }
+  } else {
+    alert("Only jpg/jpeg and png files are allowed!");
+    productPhoto.value = "";
+    imgPreview.src = "./images/img-2.png";
+    return false;
+  }
+  return true;
 }
